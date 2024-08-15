b04614048aa8eac9b8d03e11de90ad27fcc4800c
Sahilz9 <sahilyadav9857@gmail.com>
2024-08-15 23:18:47 +0530

    preamble resume

---
+++ src/app/home/QuestionsAndAnswers.tsx
@@ -0,0 +1,105 @@
+"use client"
+
+import { useState } from "react";
+import { Link } from "components/documentation";
+
+const QAS = [
+  {
+    question:
+      "Q1. What is a resume builder? Why resume builder is better than resume template doc?",
+    answer: (
+      <>
+        <p>
+          There are two ways to create a resume today. One option is to use a
+          resume template, such as an office/google doc, and customize it
+          according to your needs. The other option is to use a resume builder,
+          an online tool that allows you to input your information and
+          automatically generates a resume for you.
+        </p>
+        <p>
+          Using a resume template requires manual formatting work, like copying
+          and pasting text sections and adjusting spacing, which can be
+          time-consuming and error-prone. It is easy to run into formatting
+          issues, such as using different bullet points or font styles after
+          copying and pasting. On the other hand, a resume builder like
+          Preamble Resume saves time and prevents formatting mistakes by
+          automatically formatting the resume. It also offers the convenience of
+          easily changing font types or sizes with a simple click. In summary, a
+          resume builder is easier to use compared to a resume template.
+        </p>
+      </>
+    ),
+  },
+  {
+    question:
+      "Q2. What uniquely sets Preamble Resume apart from other resume builders and templates?",
+    answer: (
+      <>
+        <p>
+          Other than Preamble Resume, there are some great free resume builders out
+          there, e.g. <Link href="https://rxresu.me/">Reactive Resume</Link>,{" "}
+          <Link href="https://flowcv.com/">FlowCV</Link>. However, Preamble Resume
+          stands out with 2 distinctive features:
+        </p>{" "}
+        <p>
+          <span className="font-semibold">
+            1. Preamble Resume is designed specifically for the U.S. job market and
+            best practices.
+          </span>
+          <br />
+          Unlike other resume builders that target a global audience and offer
+          many customization options, Preamble Resume intentionally only offers
+          options that are aligned with U.S. best practices. For example, it
+          excludes the option to add a profile picture to avoid bias and
+          discrimination. It offers only the core sections, e.g. profile, work
+          experience, education, and skills, while omitting unnecessary sections
+          like references. Additionally, Preamble Resume only offers a top down
+          single column resume design as opposed to two column design, because
+          single column design works best for AST. <br />{" "}
+        </p>
+        <p>
+          <span className="font-semibold">
+            2. Preamble Resume is super privacy focus.
+          </span>{" "}
+          <br />
+          While other resume builders may require email sign up and store user
+          data in their databases, Preamble Resume believes that resume data should
+          remain private and accessible only on user’s local machine. Therefore,
+          Preamble Resume doesn’t require sign up to use the app, and all inputted
+          data is stored in user’s browser that only user has access to.
+        </p>
+      </>
+    ),
+  },
+];
+
+export const QuestionsAndAnswers = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  return (
+    <section className="mx-auto max-w-3xl divide-y divide-gray-300 lg:mt-4 lg:px-2">
+      <h2 className="text-center text-3xl font-bold">Questions & Answers</h2>
+      <div className="mt-6 divide-y divide-gray-300">
+        {QAS.map(({ question, answer }, index) => (
+          <div key={question} className="py-4">
+            <h3
+              className="cursor-pointer font-semibold leading-7"
+              onClick={() => toggleAccordion(index)}
+            >
+              {question}
+            </h3>
+            {activeIndex === index && (
+              <div className="mt-3 grid gap-2 leading-7 text-gray-600">
+                {answer}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
